cd5d1d70bd6b9d22be1a8de6979151168feb2280
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2023-01-20 21:10:49 +0100

    Clamp player movement

--- Assets/Scripts/Player/Mover.cs
+++ Assets/Scripts/Player/Mover.cs
@@ -6,12 +6,18 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform player;
     [SerializeField] float playerMoveSpeed;
+    [SerializeField] float clampPlayerMove;
    
     void Update()
     {
         if (player == null) return;
         Vector3 rawMove = new Vector3(0,Input.GetAxis("Vertical"));
 
-        player.position = player.position + rawMove*playerMoveSpeed*Time.deltaTime;
+        float rawPlayerYPos = Mathf.Clamp(player.position.y + rawMove.y * playerMoveSpeed * Time.deltaTime,
+            -clampPlayerMove, clampPlayerMove);
+
+        player.position = new Vector3(player.position.x, rawPlayerYPos);
+
+
     }
 }
